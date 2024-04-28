d08178f1166eaa9d069cbac5e155811dc10f0de0
Truong Nguyen <147793307+Truong-NguyenData@users.noreply.github.com>
2024-04-27 17:14:24 -0700

    Page2 change

--- split-it/src/App.js
+++ split-it/src/App.js
@@ -1,23 +1,58 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('ready');
+  const [file, setFile] = useState(null);
+
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+
+    if (typeof file === 'undefined' || file === null) return; // Check if file is undefined or null
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'test-react-uploads-unsigned');
+
+    try {
+      const response = await fetch('http://localhost:3000/upload-image', { // Fixed the URL
+        method: 'POST',
+        body: formData
+      });
+      
+      if (response.ok) {
+        console.log('Image uploaded successfully');
+        setMessage('Image uploaded successfully');
+      } else {
+        console.error('Failed to upload image');
+        setMessage('Failed to upload image');
+      }
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      setMessage('Error uploading image');
+    }
+  }
+
+  function handleOnChange(e) {
+    const target = e.target;
+    if (target.files.length > 0) {
+      setFile(target.files[0]);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>Split-it</h1>
+      
+        <form onSubmit={handleOnSubmit}>
+          <input type="file" name="image" onChange={handleOnChange} />
+          <p></p>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <p>{message}</p>
     </div>
   );
 }
