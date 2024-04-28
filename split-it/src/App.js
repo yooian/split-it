503787ef5bf05fb1dd3443e84b4827950b81d543
Truong Nguyen <147793307+Truong-NguyenData@users.noreply.github.com>
2024-04-28 00:46:47 -0700

    Fix page 2 useState

--- split-it/src/App.js
+++ split-it/src/App.js
@@ -1,49 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Page1 from "./pages/page1";
 import Page2 from "./pages/page2"; 
 import "./App.css";
-import { useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState('ready');
-  const [file, setFile] = useState(null);
-
-  async function handleOnSubmit(e) {
-    e.preventDefault();
-
-    if (typeof file === 'undefined' || file === null) return; // Check if file is undefined or null
-
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('upload_preset', 'test-react-uploads-unsigned');
-
-    try {
-      const response = await fetch('http://localhost:3001/upload-image', { // Fixed the URL
-        method: 'POST',
-        body: formData
-      });
-      
-      if (response.ok) {
-        console.log('Image uploaded successfully');
-        setMessage('Image uploaded successfully');
-      } else {
-        console.error('Failed to upload image');
-        setMessage('Failed to upload image');
-      }
-    } catch (error) {
-      console.error('Error uploading image:', error);
-      setMessage('Error uploading image');
-    }
-  }
-
-  function handleOnChange(e) {
-    const target = e.target;
-    if (target.files.length > 0) {
-      setFile(target.files[0]);
-    }
-  }
-
   return (
     <section className="hero">
       <div className="content">
