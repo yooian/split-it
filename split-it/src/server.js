ebed608da9c85c9b796d9c3dc38b31528f88b31a
Truong Nguyen <147793307+Truong-NguyenData@users.noreply.github.com>
2024-04-28 00:17:58 -0700

    Change a lot

--- split-it/src/server.js
+++ split-it/src/server.js
@@ -1,21 +1,37 @@
 const express = require('express');
+const cors = require('cors');
 const multer = require('multer');
+const bodyParser = require('body-parser');
+const { spawn } = require('child_process');
 
 const app = express();
-const upload = multer({ dest: '/uploads' });
+const upload = multer({ dest: 'uploads/' });
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // Route for image upload handling
-app.post('/upload-image', upload.single('image'), (req, res) => {
-    // Image upload for OCR
+app.post('/upload-image', upload.single('file'), (req, res) => {
+    const fileData = req.file;
 
     // Error handling
+    if (!file) {
+        return res.status(400).json({ error: 'No image file uploaded' });
+    }
+        
+    // Image upload for OCR
+    const pythonProcess = spawn('python', ['path/to/your/python_script.py', fileData]);
 
     // Return total cost
+
+    // Success response
+    console.log('Uploaded file:', file);
+    res.status(200).json({ message: 'Image uploaded successfully' });
 });
 
 
-const PORT = 3000; //process.env.PORT <- allows dynamic port config by environment (ex, heroku)
+const PORT = 3001; //process.env.PORT <- allows dynamic port config by environment (ex, heroku)
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+    console.log(`Server is running on port ${PORT}`);
 });
