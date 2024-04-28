16b0d137fa9bfc52328264ec8365a5b2e58b3db5
Ziqiang (Joe) Zhu <ziqzhu@ucdavis.edu>
2024-04-27 17:34:30 -0700

    working for 1.image

--- image-process/real-program.py
+++ image-process/real-program.py
@@ -2,8 +2,13 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+import pytesseract
+import re
+
 from skimage.filters import threshold_local
 from PIL import Image
+
+from pytesseract import Output
 
 from functions import opencv_resize, plot_rgb, plot_gray, get_receipt_contour, contour_to_rect, wrap_perspective, bw_scanner
 
@@ -11,7 +16,7 @@ def usr_input():
     # get the image
     file_name = input("Enter the file name: ")
     img = Image.open(file_name)
-    img.thumbnail((800,800), Image.ANTIALIAS)
+    # img.thumbnail((800,800), Image.ANTIALIAS)
 
     # # show the thumbnail
     # plt.imshow(img)
@@ -47,8 +52,8 @@ def usr_input():
     # Get 10 largest contours
     largest_contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
     image_with_largest_contours = cv2.drawContours(image.copy(), largest_contours, -1, (0,255,0), 3)
-    plot_rgb(image_with_largest_contours)
-    plt.show()
+    # plot_rgb(image_with_largest_contours)
+    # plt.show()
 
     return original, largest_contours, resize_ratio
 
@@ -67,3 +72,25 @@ scanned = wrap_perspective(image.copy(), contour_to_rect(receipt_contour, resize
 # plt.figure(figsize=(16,10))
 # plt.imshow(scanned)
 # plt.show()
+
+d = pytesseract.image_to_data(image, output_type=Output.DICT)
+n_boxes = len(d['level'])
+boxes = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2RGB)
+for i in range(n_boxes):
+    (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])    
+    boxes = cv2.rectangle(boxes, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+# plot_rgb(boxes)
+
+extracted_text = pytesseract.image_to_string(image)
+# print(extracted_text)
+
+
+total_pattern = r"(?i)total[\s]*[£$€]?[\s]*([0-9]*[.,][0-9]{2})"
+match = re.search(total_pattern, extracted_text)
+
+if match:
+    total_amount = match.group(1)
+    print(total_amount)
+else:
+    print("Total amount not found.")
